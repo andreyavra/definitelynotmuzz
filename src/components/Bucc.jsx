1130a5449b5ce04ece3c3545ccc8763da4176afc
Lesley <cat@Lesleys-MacBook.local>
2019-08-15 18:30:09 +1000

    THIS WORKS LOL
    hahahahahhahhahahahahahhashahashahashahahahahahah

--- src/components/Bucc.jsx
+++ src/components/Bucc.jsx
@@ -14,7 +14,7 @@ class Bucc extends React.Component {
       <div>
         <h1>This is the bucctastic page</h1>
         <a href="https://www.cse.unsw.edu.au/~richardb/" target="_blank">
-          <img src = "https://i.imgur.com/LtQMmKq.png" alt = "Attractive Chad" width = 500px style={{padding:5px; margin-top: 20px; margin-left: 150px}}>
+          <img className = "img" src = "https://i.imgur.com/LtQMmKq.png" alt = "Attractive Chad" />
         </a>
         
       </div>
